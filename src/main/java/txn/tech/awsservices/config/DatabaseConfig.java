749882a709945c315451e551d7d1cdaa9a7f0e43
txnt3ch <txntech@gmail.com>
2023-01-15 19:49:08 +0700

    Initial commit

--- src/main/java/txn/tech/awsservices/config/DatabaseConfig.java
+++ src/main/java/txn/tech/awsservices/config/DatabaseConfig.java
@@ -4,13 +4,35 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-
+// also can use @Data (lombok) for getters and setters
 @ConfigurationProperties(prefix = "database")
 @Component
-@Data
 public class DatabaseConfig {
 
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     private String url;
     private String username;
