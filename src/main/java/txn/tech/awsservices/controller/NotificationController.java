114a54cbf0c233c104bc480c734b5fd4aa102d60
txnt3ch <txntech@gmail.com>
2023-02-11 18:44:53 +0700

    Update

--- src/main/java/txn/tech/awsservices/controller/NotificationController.java
+++ src/main/java/txn/tech/awsservices/controller/NotificationController.java
@@ -7,6 +7,16 @@ import txn.tech.awsservices.dto.NotificationDto;
 import txn.tech.awsservices.service.NotificationService;
 
 import java.util.List;
+// Create entity Notification to map with database table
+// Create dto NotificationDTO as Data Transfer Object to use in REST
+// Create NotificationRepository interface that inherit JpaRepository
+// Create NotificationService to handle the logic to interact with DB. This service will use the jpa repository to do CURD
+// Create NotificationController that have notificationService instance and handle the REST API
+
+
+
+
+
 
 @RestController
 public class NotificationController {
